75cc4d1bc5aa61e870cae1e7089ecf8bc62b39eb
muhammad saqib <tiktokusr4300@gmail.com>
2025-11-02 16:47:11 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import AddTask from "./pages/AddTask";
+import Reports from "./pages/Reports";
+import ScheduleTracker from "./pages/ScheduleTracker";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <div className="min-h-screen bg-ios-bg">
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/add-task" element={<AddTask />} />
+            <Route path="/reports" element={<Reports />} />
+            <Route path="/schedule" element={<ScheduleTracker />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
+}
